75c3584f9006c7dbc072db2dc7d3c6dbaaf9b2da
cserspring <shuaiwhu@gmail.com>
2015-11-06 22:17:55 -0800

    PDF split and merge

---
+++ PDFSplitMerge/PdfUtilities.cs
@@ -0,0 +1,111 @@
+﻿namespace PDFSplitMerge
+{
+    using System;
+    using System.IO;
+    using iTextSharp.text;
+    using iTextSharp.text.pdf;
+
+    /// <summary>
+    /// To manipulate the pdf files.
+    /// </summary>
+    class PdfUtilities
+    {
+        /// <summary>
+        /// Extract a single page from PDF file.
+        /// </summary>
+        /// <param name="sourceFile">The source file.</param>
+        /// <param name="outputFile">The output file.</param>
+        /// <param name="pageNumber">The specific page number.</param>
+        public static void ExtractPage(string sourceFile, string outputFile, int pageNumber)
+        {
+            try
+            {
+                PdfReader reader = new PdfReader(sourceFile);
+                if (pageNumber > reader.NumberOfPages)
+                {
+                    Console.WriteLine("This page number is out of reach.");
+                    return;
+                }
+
+                Document document = new Document();
+                PdfCopy pdfCopy = new PdfCopy(document, new FileStream(outputFile, FileMode.Create));
+                document.Open();
+
+                PdfImportedPage importedPage = pdfCopy.GetImportedPage(reader, pageNumber);
+                pdfCopy.AddPage(importedPage);
+
+                document.Close();
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Extract pages from a pdf file.
+        /// </summary>
+        /// <param name="source">The source file.</param>
+        /// <param name="output">The output file.</param>
+        /// <param name="startPage">The start page.</param>
+        /// <param name="endPage">The last page.</param>
+        public static void ExtractPages(string source, string output, int startPage, int endPage)
+        {
+            try
+            {
+                PdfReader reader = new PdfReader(source);
+                Document document = new Document();
+
+                PdfCopy pdfCopy = new PdfCopy(document, new FileStream(output, FileMode.Create));
+
+                document.Open();
+                for (int i = startPage; i <= endPage; i++)
+                {
+                    PdfImportedPage importedPage = pdfCopy.GetImportedPage(reader, i);
+                    pdfCopy.AddPage(importedPage);
+                }
+
+                document.Close();
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Merge multiple pdf files into one pdf file.
+        /// </summary>
+        /// <param name="sources">The source files.</param>
+        /// <param name="outputFile">The output file.</param>
+        public static void Merge(string[] sources, string outputFile)
+        {
+            try
+            {
+                Document document = new Document();
+                PdfCopy pdfCopy = new PdfCopy(document, new FileStream(outputFile, FileMode.Create));
+
+                document.Open();
+                foreach (var source in sources)
+                {
+                    PdfReader reader = new PdfReader(source);
+                    for (int i = 1; i <= reader.NumberOfPages; i++)
+                    {
+                        PdfImportedPage page = pdfCopy.GetImportedPage(reader, i);
+                        pdfCopy.AddPage(page);
+                    }
+                    reader.Close();
+                }
+
+                pdfCopy.Close();
+                document.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ PDFSplitMerge/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace PDFSplitMerge
+{
+    using System;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string usage = "PDFSplitMerge.exe -split [SourceFile] [Pages] [OutputFolder]\n" +
+                                 "PDFSplitMerge.exe -merge [File1] [File2] ... [FileN] [OutputFolder]";
+            if (args[0].Equals("-usage", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine(usage);
+            }
+            else if (args[0].Equals("-split", StringComparison.OrdinalIgnoreCase))
+            {
+                string sourceFile = args[1];
+                string sourceFileNameWithExtension = sourceFile.Split(new char[] {'\\'}, StringSplitOptions.RemoveEmptyEntries).Last();
+                string sourceFileName = sourceFileNameWithExtension.Substring(0, sourceFileNameWithExtension.LastIndexOf('.'));
+
+                string outputPath = args[3].EndsWith("\\") ? args[3] : (args[3] + "\\");
+
+                string[] arr = args[2].Split(new char[]{','}, StringSplitOptions.RemoveEmptyEntries);
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    string[] startAndEnd = arr[i].Split(new char[] {'-'}, StringSplitOptions.RemoveEmptyEntries);
+                    int start = int.Parse(startAndEnd[0]);
+                    if (startAndEnd.Length > 1)
+                    {
+                        int end = int.Parse(startAndEnd[1]);
+                        PdfUtilities.ExtractPages(sourceFile, outputPath + sourceFileName + "_" + start + "_to_" + end + ".pdf", start, end);
+                    }
+                    else
+                    {
+                        PdfUtilities.ExtractPage(sourceFile, outputPath + sourceFileName + "_" + start + ".pdf", start);
+                    }
+                }
+            }
+            else if (args[0].Equals("-merge", StringComparison.OrdinalIgnoreCase))
+            {
+                string[] sources = new string[args.Length - 2];
+                string outputPath = args[3].EndsWith("\\") ? args[3] : (args[3] + "\\");
+
+                for (int i = 0; i < args.Length - 2;i++)
+                {
+                    sources[i] = args[i + 1];
+                }
+
+                PdfUtilities.Merge(sources, outputPath + "Merged" + ".pdf");
+            }
+        }
+    }
+}
